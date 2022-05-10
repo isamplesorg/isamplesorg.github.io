7a66d76e0fb1791c65bc157160560f5fb047d5be
datadavev <605409+datadavev@users.noreply.github.com>
2022-05-10 11:10:22 -0400

    Added vocab generation scripts

---
+++ scripts/generate_vocabs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Regenerates the vocabulary markdown files from the GH sources
+#
+#
+SCRIPT_FOLDER="$(dirname ${0})"
+SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata/develop/src/vocabularies/"
+SOURCES=("materialType.ttl" "sampledFeature.ttl" "specimenType.ttl")
+DEST_FOLDER="models/generated/vocabularies/"
+mkdir -p "${DEST_FOLDER}"
+for src in ${SOURCES[@]}; do
+    fname="${src%%.*}.md"
+    echo "Generating ${fname}..."
+    python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+done
+echo "Done."
+
