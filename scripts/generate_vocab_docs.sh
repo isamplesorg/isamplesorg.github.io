f7b83c9e95528a2b543b9de3b40a2ae33777c94a
datadave <605409+datadavev@users.noreply.github.com>
2023-10-12 20:20:54 -0400

    touching vocabs

--- scripts/generate_vocab_docs.sh
+++ scripts/generate_vocab_docs.sh
@@ -4,12 +4,12 @@
 #
 #
 SCRIPT_FOLDER="$(dirname ${0})"
-SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata/develop/src/vocabularies/"
-SOURCES=("materialType.ttl" "sampledFeature.ttl" "specimenType.ttl")
+SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata/relation-vocabulary/src/vocabularies/"
+SOURCES=("materialType.ttl" "sampledFeature.ttl" "specimenType.ttl" "relation-test.ttl")
 DEST_FOLDER="models/generated/vocabularies/"
 mkdir -p "${DEST_FOLDER}"
 for src in ${SOURCES[@]}; do
-    fname="${src%%.*}.md"
+    fname="${src%%.*}.qmd"
     echo "Generating ${fname}..."
     python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
 done
