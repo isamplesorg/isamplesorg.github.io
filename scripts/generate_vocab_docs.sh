610f6716856945c5197b2455575ddee36846abed
Stephen Richard <smrTucson@gmail.com>
2024-01-03 21:05:02 -0700

    configure to get vocab_tools, and run with current vocabs

--- scripts/generate_vocab_docs.sh
+++ scripts/generate_vocab_docs.sh
@@ -4,14 +4,18 @@
 #
 #
 SCRIPT_FOLDER="$(dirname ${0})"
-SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata/develop/src/vocabularies/"
-SOURCES=("materialType.ttl" "sampledFeature.ttl" "materialSampleType.ttl")
+#SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata/develop/src/vocabularies/"
+
+SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/vocabularies/main/vocabulary/"
+
+SOURCES=("material_type.ttl" "sampled_feature_type.ttl" "material_sample_type.ttl")
 DEST_FOLDER="models/generated/vocabularies/"
 mkdir -p "${DEST_FOLDER}"
 for src in ${SOURCES[@]}; do
     fname="${src%%.*}.md"
     echo "Generating ${fname}..."
-    python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+#    python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+    vocab markdown "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
 done
 echo "Done."
 
