6b3475c8ce51edafa9b30ea6ebf53f3d83b16b26
Stephen Richard <smrTucson@gmail.com>
2024-01-04 17:49:21 -0700

    Configure to add extension vocabs in models page
    try configuring to get extension vocabularies
    
    try putting in a listing title
    
    try to label lists
    
    remove comma in sources list
    
    remove 'title' in extension-listing qmd config
    
    remove plantuml install, change to use smrgeoinfo repo vocab_tools

--- scripts/generate_vocab_docs.sh
+++ scripts/generate_vocab_docs.sh
@@ -17,5 +17,39 @@ for src in ${SOURCES[@]}; do
 #    python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
     vocab markdown "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
 done
+
+SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata_profile_earth_science/main/vocabulary/"
+SOURCES=("earthenv_material_extension_mineral_group.ttl" "earthenv_material_extension_rock_sediment.ttl" "earthenv_sampled_feature_role.ttl" "earthenv_specimen_type.ttl")
+DEST_FOLDER="models/generated/extensions/"
+mkdir -p "${DEST_FOLDER}"
+for src in ${SOURCES[@]}; do
+    fname="${src%%.*}.md"
+    echo "Generating ${fname}..."
+#    python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+    vocab markdown "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+done
+
+SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata_profile_archaeology/main/vocabulary/"
+SOURCES=("opencontext_material_extension.ttl" "opencontext_materialsampletype.ttl")
+DEST_FOLDER="models/generated/extensions/"
+mkdir -p "${DEST_FOLDER}"
+for src in ${SOURCES[@]}; do
+    fname="${src%%.*}.md"
+    echo "Generating ${fname}..."
+#    python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+    vocab markdown "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+done
+
+SOURCE_BASE="https://raw.githubusercontent.com/isamplesorg/metadata_profile_biology/main/vocabulary/"
+SOURCES=("biology_sampledfeature_extension.ttl")
+DEST_FOLDER="models/generated/extensions/"
+mkdir -p "${DEST_FOLDER}"
+for src in ${SOURCES[@]}; do
+    fname="${src%%.*}.md"
+    echo "Generating ${fname}..."
+#    python "${SCRIPT_FOLDER}/vocab2md.py" "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+    vocab markdown "${SOURCE_BASE}${src}" > "${DEST_FOLDER}${fname}"
+done
+
 echo "Done."
 
